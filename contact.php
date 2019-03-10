4853740bf7b60d0134355631cc21e754355e10dd
Shane Gibney <shanegibney@Shanes-MacBook-Pro.local>
2019-03-10 23:13:44 +0000

    removed unnecessary directories

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@ Currently editing:
 
 // echo "blah1234567890";
 // an email address that will be in the From field of the email.
-$from = 'admin@glenalbynmasters.com';
+$from = 'admin@los.com';
 
 // // an email address that will receive the email with the output of the form
 // $sendTo = 'Demo contact form <demo@domain.com>';
@@ -19,14 +19,14 @@ $from = 'admin@glenalbynmasters.com';
 //
 // // subject of the email
 // $subject = 'New message from contact form';
-$subject = "Glenalbynmasters.com contact form message";
+$subject = "Leinster Open Sea contact form message";
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 // message that will be displayed when everything is OK :)
 
-$okMessage = 'Contact form successfully submitted to Glenalbyn Masters admin. </br></br>Thank you, </br></br>We will get back to you soon!</br>
+$okMessage = 'Contact form successfully submitted to LOS admin. </br></br>Thank you, </br></br>We will get back to you soon!</br>
 </br><a href="http://www.glenalbynmasters.com">Return to home page</a>';
 
 // If something goes wrong, we will display this message.
@@ -44,7 +44,7 @@ try
 
     if(count($_POST) == 0) throw new \Exception('Form is empty');
 
-    $emailText = "You have a new message from the Glenalbyn Masters contact form\n\n";
+    $emailText = "You have a new message from the LOS contact form\n\n";
 
     foreach ($_POST as $key => $value) {
         // If the field exists in the $fields array, include it in the email
@@ -61,7 +61,7 @@ try
     );
 
     // Send email
-    mail('carobrennan@yahoo.com , declan.culliton@yahoo.com , doyleei@tcd.ie , gibneysh@tcd.ie , janet@facit.ie ', $subject, $emailText, implode("\n", $headers));
+    mail('leinsteropenseaswim@gmail.com', $subject, $emailText, implode("\n", $headers));
     //mail('gibneysh@tcd.ie , shanegibney@gmail.com ', $subject, $emailText, implode("\n", $headers));
 
     echo "</br>";
